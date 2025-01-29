3ef852259307cd7f6184f72b3ef7fd48e5a65a2a
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2025-01-28 21:16:06 -0700

    typo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { initializeBranches } from './branches';
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div id="warning">under construction: shamelessly derived from mr doob's branches</div>
+  <div id="warning">under construction</div>
   <div id="info">click anywhere</div>
   <div id="container"></div>
 `;
